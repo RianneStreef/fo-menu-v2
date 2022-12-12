82968088cd13e6e9a4042038031c1928ede6069e
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-12-12 14:09:59 +0100

    additional packages manifest and google tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,42 @@
 module.exports = {
   siteMetadata: {
     title: `fo-menu-v2`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.menu-lafaceouest.com`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-ZZDV8FZKTE"],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+        // defaults to false
+        enableWebVitalsTracking: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
 };
